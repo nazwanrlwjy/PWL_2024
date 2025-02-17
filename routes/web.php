c56cca1f50af4b844b3bda0c0d9b7c27f0edd9bd
Nazwa Nurul Wijaya <nazwanrlwjy@gmail.com>
2025-02-17 14:29:13 +0700

    JS2_2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+    
+
+Route::get('/about', function () {
+    $nim = '2341760045'; 
+    $nama = 'Nazwa Nurul Wijaya'; 
+    return "NIM: $nim <br> Nama: $nama";
+});
+
+Route::get('/selamat', function () {
+    return 'Selamat Datang';
+ });
+ 
+
+Route::get('/world', function () {
+    return 'World';
+ });
+ 
+Route::get('/hello', function () {
+    return 'Hello World';
+ });
+
+ 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
